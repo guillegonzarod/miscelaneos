3cf30f468370c287607ef5f8bd58ad348d041927
Guille <info@guillegonzarod.com>
2019-01-21 00:07:09 +0100

    Demostración del ciclo de vida de una página.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -6,11 +7,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Pagina2Page } from './../pages/pagina2/pagina2';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    Pagina2Page
   ],
   imports: [
     BrowserModule,
@@ -19,7 +22,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    Pagina2Page
   ],
   providers: [
     StatusBar,
---
+++ src/pages/pagina2/pagina2.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-pagina2',
+  templateUrl: 'pagina2.html',
+})
+export class Pagina2Page {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ir_pagina3() {
+    this.navCtrl.push("mi-pagina3");
+  }
+
+}
