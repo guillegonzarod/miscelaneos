8a1bca99df492deb4c308c14b29d7113b7fc2e8e
Guille <info@guillegonzarod.com>
2019-01-21 20:58:05 +0100

    Demostración del ciclo de vida de una página.

--- src/pages/pagina2/pagina2.ts
+++ src/pages/pagina2/pagina2.ts
@@ -15,4 +15,50 @@ export class Pagina2Page {
     this.navCtrl.push("mi-pagina3");
   }
 
+  ionViewCanEnter() {
+    console.log("ionViewCanEnter");
+    let numero = Math.round(Math.random() * 10);
+    console.log(numero);
+    if (numero >= 3) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  ionViewDidLoad() {
+    console.log("ionViewDidLoad");
+  }
+
+  ionViewWillEnter() {
+    console.log("ionViewWillEnter");
+  }
+
+  ionViewDidEnter() {
+    console.log("ionViewDidEnter");
+  }
+
+  ionViewCanLeave() {
+    console.log("ionViewCanLeave");
+    console.log("Espera 2 segundos para salir");
+    let promesa = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(true);
+      }, 2000);
+    });
+
+    return promesa;
+  }
+
+  ionViewWillLeave() {
+    console.log("ionViewWillLeave");
+  }
+
+  ionViewDidLeave() {
+    console.log("ionViewDidLeave");
+  }
+
+  ionViewWillUnload() {
+    console.log("ionViewWillUnload");
+  }
 }
